a35dfa212390d4d9396f3fcfa7e5e59ffe64c853
shivam dubey <shivamdubey88016@gmail.com>
2025-05-13 23:51:02 +0530

    updated version

--- app.js
+++ app.js
@@ -2,19 +2,22 @@ const express= require('express');
 const app= express();
 const mongoose= require('mongoose');
 const path= require('path');
+const listingSchema=require('./schema.js');
 //setting up ejs-mate
 const ejsmate= require('ejs-mate');
 app.engine('ejs', ejsmate);
 app.use(express.static(path.join(__dirname, '/public')));
 const wrapAsync= require('./utils/wrap.js');
-const xpressError = require("./utils/ExpressError.js");
+const ExpressError = require("./utils/ExpressError.js");
 //setting up view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended:true}));
 const listing=require("./models/listing.js");
+const review=require("./models/review.js");
 const methodOverride = require("method-override");
 app.use(methodOverride("_method"));
+//const{ reviewSchema}=require('./schema.js');
 
 
 
@@ -24,14 +27,23 @@ async function main() {
     await mongoose.connect(mongoUrl);}
 main().then(() => {console.log("connected to database");})
 .catch(err => {console.log(err);});
-
-
+/*validation of review
+const validateReview=(req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
+    if (error) {
+      const msg = error.details.map((el) => el.message).join(",");
+      throw new ExpressError(msg, 400);
+    } else {
+      next();
+    }
+  };*/
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 
 }
 );
+
 app.get('/listings', async (req, res) => {
     const alllistings=await listing.find({})
         
@@ -46,7 +58,8 @@ app.get('/listings', async (req, res) => {
    });
    // create listing route
    app.post("/listings",wrapAsync(async(req, res) => {
-
+listingSchema.validate(req.body);
+console.log(req.body);
    
     const newlisting = new listing(req.body.listing);
     await newlisting.save();
@@ -82,27 +95,50 @@ app.get("/listings/:id/edit", async (req, res) => {
     res.redirect("/listings");
   });
 //show route
-  app.get("/listings/:id", async (req, res) => {
+ app.get("/listings/:id", async (req, res) => {
     let { id } = req.params;
-    const listingl = await listing.findById(id);
+    const listingl = await listing.findById(id).populate("reviews");
+    
     res.render("listings/show.ejs", { listingl });
-  }); 
- 
-
-
-  app.use((err, req, res, next) => {
-    let { statusCode , message  } = err;
-   res.render("error.ejs");
   });
+
+  //reviews route
+  app.post("/listings/:id/reviews", wrapAsync(async (req, res) => {
+  const { id } = req.params;
+  const listingl = await listing.findById(id);
+
+  // Extract data directly from req.body
+  const { rating, comment,name } = req.body;
+
+  const review1 = new review({ rating, comment,name }); 
+  listingl.reviews.push(review1); 
+  await review1.save(); 
+  await listingl.save();
+
+  res.redirect(`/listings/${id}`);
+}));
+
+app.delete("/listings/:id/reviews/:reviewId", async (req, res) => {
+    const { id, reviewId } = req.params;
+    await listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await review.findByIdAndDelete(reviewId);
+    res.redirect(`/listings/${id}`); 
+
+   });
+
+ /*app.use((err, req, res, next) => {
+    //let { statusCode , message  } = err;
+  // res.render("error.ejs");
+ // });*/
   //our middleware
   
-    
-
-
+   
   app.listen(3000, () =>{
     console.log("server is running on port 3000");
 }
 );
  app.all("",(req, res,next) => {
-    next(new xpressError("Page not found", 404));
+    next(new ExpressError("Page not found", 404));
   });
+
+  
